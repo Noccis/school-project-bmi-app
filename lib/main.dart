ad0112c2597563709c052603772fee233fe94569
Toni <89582846+Noccis@users.noreply.github.com>
2022-08-26 01:36:18 -0700

    added reset button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/rendering.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,8 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -33,10 +33,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   double _bmiNumber = 0.0;
   String _kategori = '';
-  
+  bool _reset = true;
 
   // Controllers:
   final heightController = TextEditingController();
@@ -49,34 +48,39 @@ class _MyHomePageState extends State<MyHomePage> {
     final height = double.parse(heightInputText);
     final weight = double.parse(weightInputText);
 
-    final double result = weight / pow(height / 100,2);
+    final double result = weight / pow(height / 100, 2);
     print('calculate Bmi running');
-    
-    
+
     setState(() {
       _bmiNumber = double.parse(result.toStringAsFixed(1));
-      heightController.text = '';
-      weightController.text = '';
-
-      if(result <= 18.5){
-      _kategori = 'underviktig';
-      }else if(result > 18.5 && result <= 24.9){
+      
+      if (result <= 18.5) {
+        _kategori = 'underviktig';
+      } else if (result > 18.5 && result <= 24.9) {
         _kategori = 'Ok enligt samhället';
-      }else if(result >24.9 && result <=29.9){
+      } else if (result > 24.9 && result <= 29.9) {
         _kategori = 'Överviktig';
-      }else if(result > 29.9 && result <= 34.9){
+      } else if (result > 29.9 && result <= 34.9) {
         _kategori = 'Fetma';
-      }else if(result >34.9 && result > 35){
+      } else if (result > 34.9 && result > 35) {
         _kategori = 'Extrem fetma';
-      }else{
+      } else {
         _kategori = 'Försök igen';
       }
-      
+      _reset = false;
     });
   }
-  
 
-  
+  void _resetBmi(){
+    setState(() {
+      heightController.text = '';
+      weightController.text = '';
+      _bmiNumber = 0;
+      _kategori = '';
+      _reset = true;
+    });
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -94,42 +98,45 @@ class _MyHomePageState extends State<MyHomePage> {
                 width: 300,
                 height: 200,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  Text('$_bmiNumber',
-                  style: const TextStyle(fontSize: 40),),
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Text(_kategori),
-                ]),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '$_bmiNumber',
+                        style: const TextStyle(fontSize: 40),
+                      ),
+                      const Padding(padding: EdgeInsets.all(8)),
+                      Text(_kategori),
+                    ]),
               ),
             ),
-            Column(children: <Widget>[
-              TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter height:'
-              ),
-              controller: heightController,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter weight:'
-              ),
-              controller: weightController,
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            ElevatedButton(
-              onPressed: _calculateBmi, 
-              child: const Text('Calculate'),
-              ),
-            ],)
-            
+            Column(
+              children: <Widget>[
+                TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Skriv in längd:'),
+                  controller: heightController,
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Skriv in vikt:'),
+                  controller: weightController,
+                ),
+                const Padding(padding: EdgeInsets.all(10)),
+                _reset
+                    ? ElevatedButton(
+                        onPressed: _calculateBmi,
+                        child: const Text('Räkna ut BMI'),
+                      )
+                    : ElevatedButton(
+                        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.red)),
+                        onPressed: _resetBmi,
+                        child: const Text('Börja om'),
+                      ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
